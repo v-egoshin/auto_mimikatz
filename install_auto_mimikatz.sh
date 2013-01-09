0c42530ebad60e1faa783817015d9891324f0a57
Plum <v@egosh.in>
2013-01-09 18:55:22 +0400

    Installing all required modules for mimikatz

---
+++ install_auto_mimikatz.sh
@@ -0,0 +1,34 @@
+mkdir /tmp/Development #download directory
+cd /tmp/Development
+
+# copy copy
+git clone https://github.com/darkoperator/Metasploit-Plugins.git msf_plugins
+git clone https://github.com/darkoperator/Meterpreter-Scripts.git msf_modules
+
+rm -rf msf_plugins/pentest.rb # dirty hack
+
+mv msf_plugins/ ~/.msf4/plugins
+mv msf_modules/ ~/.msf4/modules
+
+wget https://raw.github.com/mandreko/metasploit-framework/mimikatz_post/modules/post/windows/gather/mimikatz.rb
+sleep 5
+mv mimikatz.rb ~/.msf4/modules/post/windows/gather/mimikatz.rb
+
+wget http://blog.gentilkiwi.com/downloads/mimikatz_trunk.zip
+unzip mimikatz_trunk.zip -d mimikatz_trunk
+
+mv mimikatz_trunk/Win32/mimikatz.exe /opt/metasploit/msf3/data/post/mimikatz-x86.exe
+mv mimikatz_trunk/Win32/sekurlsa.dll /opt/metasploit/msf3/data/post/sekurlsa-x86.dll
+
+mv mimikatz_trunk/x64/mimikatz.exe /opt/metasploit/msf3/data/post/mimikatz-x64.exe
+mv mimikatz_trunk/x64/sekurlsa.dll /opt/metasploit/msf3/data/post/sekurlsa-x64.dll
+# delete all
+cd / && rm -rf /tmp/Development
+
+
+rm -rf ~/.msf4/plugins
+rm -rf ~/.msf4/modules
+rm -rf /opt/metasploit/msf3/data/post/mimikatz*
+rm -rf /opt/metasploit/msf3/data/post/sekurlsa*
+
+
